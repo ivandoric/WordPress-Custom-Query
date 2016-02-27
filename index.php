54bf9444e398c373bb19f5f4d877c60f639a0785
Ivan Doric <ivan.doric@gauss.hr>
2016-02-27 12:23:26 +0100

    WordPress Custom Query - Part 04 - Taxonomy

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php get_header(); ?>
 
 	<div class="container">
-        <h4 class="lesson-title">WP Custom Query - Comparing</h4>
+        <h4 class="lesson-title">WP Custom Query - Taxonomy</h4>
         <?php
                 $args = array(
                     'post_type' => 'post',
-                    'posts_per_page' => 20,
-                    'meta_key' => 'name',
-                    'meta_compare' => '!=',
-                    'meta_value' => 'John', 
-                    'orderby' => 'meta_value',
-                    'order' => 'ASC'
+                    'posts_per_page' => -1,
+                    'tag_slug__in' => array('craft','sass')
                 );
                 $query = new WP_Query($args);
                 while($query -> have_posts()) : $query -> the_post();
         ?>
-            <h5><?php the_title(); ?></h5>
+            <div class="post clearfix">
+                <h5><?php the_title(); ?></h5>
+                <div class="taxonomy clearfix">
+                    <div class="categories">
+                        <strong>Categories:</strong>
+                        <?php the_category() ?>
+                    </div>
+                
+                    <div class="tags">
+                        <?php the_tags(); ?>
+                    </div>
+                </div>
+            </div>
         <?php endwhile; wp_reset_query(); ?>
     </div>
 

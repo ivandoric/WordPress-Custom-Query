807c168c5c4b2d79fc1960fb0127050c45da6363
Ivan Doric <ivan.doric@gauss.hr>
2016-03-01 18:25:16 +0100

    WordPress Custom Query - Part 05 - meta_query

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
 <?php get_header(); ?>
 
 	<div class="container">
-        <h4 class="lesson-title">WP Custom Query - Taxonomy</h4>
+        <h4 class="lesson-title">WP Custom Query - meta_query</h4>
         <?php
                 $args = array(
                     'post_type' => 'post',
                     'posts_per_page' => -1,
-                    'tag_slug__in' => array('craft','sass')
+                    'meta_query' => array(
+                        array(
+                            'relation' => 'OR',
+                            array(
+                                'key' => 'size',
+                                'value' => 'l',
+                                'type' => 'CHAR',
+                                'compare' => '='
+                            ),
+                            array(
+                                'key' => 'color',
+                                'value' => 'green',
+                                'type' => 'CHAR',
+                                'compare' => '='
+                            )
+
+                        ),
+
+                        array(
+                            'key' => 'price',
+                            'value' => 100,
+                            'type' => 'NUMERIC',
+                            'compare' => '<'
+                        )
+                    )
                 );
                 $query = new WP_Query($args);
                 while($query -> have_posts()) : $query -> the_post();
@@ -15,12 +39,13 @@
                 <h5><?php the_title(); ?></h5>
                 <div class="taxonomy clearfix">
                     <div class="categories">
-                        <strong>Categories:</strong>
-                        <?php the_category() ?>
+                        <strong>Price:</strong>
+                        <span class="price"><?php the_field('price') ?></span>
                     </div>
                 
                     <div class="tags">
-                        <?php the_tags(); ?>
+                        <strong>Size:</strong> <?php the_field('size'); ?><br>
+                        <strong>Color:</strong> <?php the_field('color'); ?>
                     </div>
                 </div>
             </div>
